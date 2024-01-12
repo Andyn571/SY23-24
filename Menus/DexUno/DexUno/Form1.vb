1fbcd1eb7d1a621fb4d08483a9e12c59448fc63f
Andyn <ajkou571@learner.dvusd.org>
2024-01-12 12:41:37 -0700

    Read Data FIle

--- Menus/DexUno/DexUno/Form1.vb
+++ Menus/DexUno/DexUno/Form1.vb
@@ -1,4 +1,8 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
+    Dim Records(50) As String
+
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         Field1.Text = ""
         Field2.Text = ""
@@ -30,6 +34,14 @@
 
     Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
         PictureBox1.Load(OpenFileDialog1.FileName)
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If IO.File.Exists("data.txt") Then
+            Dim inFile As New StreamReader("data.txt")
+            Records(0) = inFile.ReadLine
+            Records(1) = inFile.ReadLine
+            inFile.Close()
+        End If
     End Sub
 End Class
