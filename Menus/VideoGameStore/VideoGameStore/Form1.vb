3e6427d0ab01174dddbc21ce40245ab47f2e0534
Colby <Ciash155@learner.dvusd.org>
2024-03-21 13:52:13 -0700

    2ND PUSH

--- Menus/VideoGameStore/VideoGameStore/Form1.vb
+++ Menus/VideoGameStore/VideoGameStore/Form1.vb
@@ -25,4 +25,8 @@ Public Class Form1
         Me.Show()
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
