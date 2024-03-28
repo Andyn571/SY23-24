f1f3d63ed03db8b61446f66e75604bba9ecaee17
Andyn <ajkou571@learner.dvusd.org>
2024-03-28 13:55:38 -0700

    pizza pockets

--- Menus/VideoGameStore/VideoGameStore/Form1.Designer.vb
+++ Menus/VideoGameStore/VideoGameStore/Form1.Designer.vb
@@ -34,11 +34,32 @@ Partial Class Form1
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Field1 = New System.Windows.Forms.TextBox()
+        Me.Field2 = New System.Windows.Forms.TextBox()
+        Me.Field3 = New System.Windows.Forms.TextBox()
+        Me.Field4 = New System.Windows.Forms.TextBox()
+        Me.Field5 = New System.Windows.Forms.TextBox()
+        Me.Field6 = New System.Windows.Forms.TextBox()
+        Me.Field7 = New System.Windows.Forms.TextBox()
+        Me.Field8 = New System.Windows.Forms.TextBox()
+        Me.Field9 = New System.Windows.Forms.TextBox()
+        Me.Field10 = New System.Windows.Forms.TextBox()
+        Me.Field11 = New System.Windows.Forms.TextBox()
+        Me.Field12 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         CType(Me.OurConsolesImage, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'StoreName
@@ -46,7 +67,7 @@ Partial Class Form1
         Me.StoreName.AutoSize = True
         Me.StoreName.Font = New System.Drawing.Font("Goudy Stout", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.StoreName.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.StoreName.Location = New System.Drawing.Point(221, 26)
+        Me.StoreName.Location = New System.Drawing.Point(271, 26)
         Me.StoreName.Name = "StoreName"
         Me.StoreName.Size = New System.Drawing.Size(465, 39)
         Me.StoreName.TabIndex = 0
@@ -57,7 +78,7 @@ Partial Class Form1
         Me.CartButton.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.CartButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
         Me.CartButton.Font = New System.Drawing.Font("Algerian", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.CartButton.Location = New System.Drawing.Point(795, 12)
+        Me.CartButton.Location = New System.Drawing.Point(836, 12)
         Me.CartButton.Name = "CartButton"
         Me.CartButton.Size = New System.Drawing.Size(143, 53)
         Me.CartButton.TabIndex = 1
@@ -71,7 +92,7 @@ Partial Class Form1
         Me.TextBox1.Location = New System.Drawing.Point(27, 83)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(898, 95)
+        Me.TextBox1.Size = New System.Drawing.Size(952, 95)
         Me.TextBox1.TabIndex = 2
         Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
@@ -80,7 +101,7 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Goudy Stout", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(343, 209)
+        Me.Label1.Location = New System.Drawing.Point(372, 209)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(280, 27)
         Me.Label1.TabIndex = 3
@@ -89,9 +110,9 @@ Partial Class Form1
         'OurConsolesImage
         '
         Me.OurConsolesImage.BackColor = System.Drawing.Color.White
-        Me.OurConsolesImage.Location = New System.Drawing.Point(27, 239)
+        Me.OurConsolesImage.Location = New System.Drawing.Point(12, 239)
         Me.OurConsolesImage.Name = "OurConsolesImage"
-        Me.OurConsolesImage.Size = New System.Drawing.Size(898, 484)
+        Me.OurConsolesImage.Size = New System.Drawing.Size(984, 484)
         Me.OurConsolesImage.TabIndex = 4
         Me.OurConsolesImage.TabStop = False
         Me.OurConsolesImage.Tag = ""
@@ -125,7 +146,7 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(45, 262)
+        Me.PictureBox1.Location = New System.Drawing.Point(121, 261)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(127, 110)
         Me.PictureBox1.TabIndex = 8
@@ -133,7 +154,7 @@ Partial Class Form1
         '
         'PictureBox2
         '
-        Me.PictureBox2.Location = New System.Drawing.Point(288, 262)
+        Me.PictureBox2.Location = New System.Drawing.Point(364, 261)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(127, 110)
         Me.PictureBox2.TabIndex = 9
@@ -141,7 +162,7 @@ Partial Class Form1
         '
         'PictureBox3
         '
-        Me.PictureBox3.Location = New System.Drawing.Point(531, 262)
+        Me.PictureBox3.Location = New System.Drawing.Point(607, 261)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(127, 110)
         Me.PictureBox3.TabIndex = 10
@@ -149,18 +170,179 @@ Partial Class Form1
         '
         'PictureBox4
         '
-        Me.PictureBox4.Location = New System.Drawing.Point(774, 262)
+        Me.PictureBox4.Location = New System.Drawing.Point(850, 261)
         Me.PictureBox4.Name = "PictureBox4"
         Me.PictureBox4.Size = New System.Drawing.Size(127, 110)
         Me.PictureBox4.TabIndex = 11
         Me.PictureBox4.TabStop = False
+        '
+        'Field1
+        '
+        Me.Field1.Location = New System.Drawing.Point(121, 377)
+        Me.Field1.Name = "Field1"
+        Me.Field1.Size = New System.Drawing.Size(127, 27)
+        Me.Field1.TabIndex = 12
+        '
+        'Field2
+        '
+        Me.Field2.Location = New System.Drawing.Point(364, 377)
+        Me.Field2.Name = "Field2"
+        Me.Field2.Size = New System.Drawing.Size(127, 27)
+        Me.Field2.TabIndex = 13
+        '
+        'Field3
+        '
+        Me.Field3.Location = New System.Drawing.Point(607, 377)
+        Me.Field3.Name = "Field3"
+        Me.Field3.Size = New System.Drawing.Size(127, 27)
+        Me.Field3.TabIndex = 14
+        '
+        'Field4
+        '
+        Me.Field4.Location = New System.Drawing.Point(850, 377)
+        Me.Field4.Name = "Field4"
+        Me.Field4.Size = New System.Drawing.Size(127, 27)
+        Me.Field4.TabIndex = 15
+        '
+        'Field5
+        '
+        Me.Field5.Location = New System.Drawing.Point(121, 410)
+        Me.Field5.Name = "Field5"
+        Me.Field5.Size = New System.Drawing.Size(127, 27)
+        Me.Field5.TabIndex = 19
+        '
+        'Field6
+        '
+        Me.Field6.Location = New System.Drawing.Point(364, 410)
+        Me.Field6.Name = "Field6"
+        Me.Field6.Size = New System.Drawing.Size(127, 27)
+        Me.Field6.TabIndex = 18
+        '
+        'Field7
+        '
+        Me.Field7.Location = New System.Drawing.Point(607, 410)
+        Me.Field7.Name = "Field7"
+        Me.Field7.Size = New System.Drawing.Size(127, 27)
+        Me.Field7.TabIndex = 17
+        '
+        'Field8
+        '
+        Me.Field8.Location = New System.Drawing.Point(850, 410)
+        Me.Field8.Name = "Field8"
+        Me.Field8.Size = New System.Drawing.Size(127, 27)
+        Me.Field8.TabIndex = 16
+        '
+        'Field9
+        '
+        Me.Field9.Location = New System.Drawing.Point(121, 443)
+        Me.Field9.Name = "Field9"
+        Me.Field9.Size = New System.Drawing.Size(127, 27)
+        Me.Field9.TabIndex = 23
+        '
+        'Field10
+        '
+        Me.Field10.Location = New System.Drawing.Point(364, 443)
+        Me.Field10.Name = "Field10"
+        Me.Field10.Size = New System.Drawing.Size(127, 27)
+        Me.Field10.TabIndex = 22
+        '
+        'Field11
+        '
+        Me.Field11.Location = New System.Drawing.Point(607, 443)
+        Me.Field11.Name = "Field11"
+        Me.Field11.Size = New System.Drawing.Size(127, 27)
+        Me.Field11.TabIndex = 21
+        '
+        'Field12
+        '
+        Me.Field12.Location = New System.Drawing.Point(850, 443)
+        Me.Field12.Name = "Field12"
+        Me.Field12.Size = New System.Drawing.Size(127, 27)
+        Me.Field12.TabIndex = 20
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 377)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 20)
+        Me.Label2.TabIndex = 24
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 410)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 20)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Price"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 443)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 20)
+        Me.Label4.TabIndex = 26
+        Me.Label4.Text = "Description"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+        Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.None
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(3, 1)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(54, 28)
+        Me.MenuStrip1.TabIndex = 27
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveToolStripMenuItem, Me.NewToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.RosyBrown
-        Me.ClientSize = New System.Drawing.Size(950, 754)
+        Me.ClientSize = New System.Drawing.Size(1008, 754)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Field9)
+        Me.Controls.Add(Me.Field10)
+        Me.Controls.Add(Me.Field11)
+        Me.Controls.Add(Me.Field12)
+        Me.Controls.Add(Me.Field5)
+        Me.Controls.Add(Me.Field6)
+        Me.Controls.Add(Me.Field7)
+        Me.Controls.Add(Me.Field8)
+        Me.Controls.Add(Me.Field4)
+        Me.Controls.Add(Me.Field3)
+        Me.Controls.Add(Me.Field2)
+        Me.Controls.Add(Me.Field1)
         Me.Controls.Add(Me.PictureBox4)
         Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.PictureBox2)
@@ -173,6 +355,8 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.CartButton)
         Me.Controls.Add(Me.StoreName)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.OurConsolesImage, System.ComponentModel.ISupportInitialize).EndInit()
@@ -180,6 +364,8 @@ Partial Class Form1
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -197,4 +383,24 @@ Partial Class Form1
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Field1 As TextBox
+    Friend WithEvents Field2 As TextBox
+    Friend WithEvents Field3 As TextBox
+    Friend WithEvents Field4 As TextBox
+    Friend WithEvents Field5 As TextBox
+    Friend WithEvents Field6 As TextBox
+    Friend WithEvents Field7 As TextBox
+    Friend WithEvents Field8 As TextBox
+    Friend WithEvents Field9 As TextBox
+    Friend WithEvents Field10 As TextBox
+    Friend WithEvents Field11 As TextBox
+    Friend WithEvents Field12 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- Menus/VideoGameStore/VideoGameStore/Form1.vb
+++ Menus/VideoGameStore/VideoGameStore/Form1.vb
@@ -1,20 +1,143 @@
+Imports System.IO
 Imports System.Reflection.Emit
 
 Public Class Form1
+    Dim records(50) As String
+    Dim count As Integer
+    Dim current As Integer
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
+        SaveToFile()
+        If count > 0 Then
+            current = count - 1
+            ShowRecord(current)
+        End If
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        SaveToFile()
+        If current < count - 1 Then
+            current = current + 1
+        End If
+        ShowRecord(current)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-
+        SaveToFile()
+        current = 0
+        ShowRecord(current)
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Field1.Text = ""
+        Field2.Text = ""
+        Field3.Text = ""
+        Field4.Text = ""
+        Field5.Text = ""
+        PictureBox1.Image = Nothing
+        PictureBox2.Image = Nothing
+        PictureBox3.Image = Nothing
+        PictureBox4.Image = Nothing
+        current = count
+        count = count + 1
+    End Sub
 
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        PictureBox1.Load(OpenFileDialog1.FileName)
+        PictureBox2.Load(OpenFileDialog1.FileName)
+        PictureBox3.Load(OpenFileDialog1.FileName)
+        PictureBox4.Load(OpenFileDialog1.FileName)
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveToFile()
+    End Sub
+    Sub SaveToFile()
+        Dim r As String
+        r += Field1.Text
+        r += "|"
+        r += Field2.Text
+        r += "|"
+        r += Field3.Text
+        r += "|"
+        r += Field4.Text
+        r += "|"
+        r += Field5.Text
+        r += "|"
+        r += Field6.Text
+        r += "|"
+        r += Field7.Text
+        r += "|"
+        r += Field8.Text
+        r += "|"
+        r += Field9.Text
+        r += "|"
+        r += Field10.Text
+        r += "|"
+        r += Field11.Text
+        r += "|"
+        r += Field12.Text
+        r += "|"
+        r += PictureBox1.ImageLocation
+        r += PictureBox2.ImageLocation
+        r += PictureBox3.ImageLocation
+        r += PictureBox4.ImageLocation
+        If count = 0 Then count = 1
+        records(current) = r
+
+        Dim outFile As New StreamWriter("data.txt")
+        For index = 0 To count - 1
+            outFile.WriteLine(records(index))
+        Next
+        outFile.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If IO.File.Exists("data.txt") Then
+            Dim inFile As New StreamReader("data.txt")
+            While (Not inFile.EndOfStream)
+                records(count) = inFile.ReadLine
+                count = count + 1
+            End While
+            inFile.Close()
+            ShowRecord(0)
+        End If
+    End Sub
+    Public Sub ShowRecord(index As Integer)
+        PictureBox1.Image = Nothing
+        If records(index) <> Nothing Then
+            Dim Fields() As String
+            Fields = records(index).Split("|")
+            Field1.Text = Fields(0)
+            Field2.Text = Fields(1)
+            Field3.Text = Fields(2)
+            Field4.Text = Fields(3)
+            Field5.Text = Fields(4)
+            Field6.Text = Fields(5)
+            Field7.Text = Fields(6)
+            Field8.Text = Fields(7)
+            Field9.Text = Fields(8)
+            Field10.Text = Fields(9)
+            Field11.Text = Fields(10)
+            Field12.Text = Fields(11)
+            If File.Exists(Fields(11)) Then
+                PictureBox1.Load(Fields(11))
+                PictureBox2.Load(Fields(11))
+                PictureBox3.Load(Fields(11))
+                PictureBox4.Load(Fields(11))
+            End If
+        End If
     End Sub
 End Class
